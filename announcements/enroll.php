cf745fe4a2772c99b678cbef4f58733ae90377c7
chukwumaokere <chukwumaokere@yahoo.com>
2019-07-26 16:15:01 +0000

    updating code for webserver handling json requests

--- announcements/enroll.php
+++ announcements/enroll.php
@@ -1,4 +1,16 @@
 <?php
+$inputJSON = file_get_contents("php://input");
+$j = json_decode($inputJSON, true);
+$r = print_r(json_decode($inputJSON, true),true);
+
+$challenge = $_REQUEST['hub_challenge'];
+$challenge_is = "";
+if($challenge){
+    header("HTTP/1.1 200");
+    echo $challenge;
+    $challenge_is = "Challenge is: $challenge\n";
+}
+/*
 include("../../../db_twitch.php");
 include("../../../db_discord.php");
 global $db, $cs, $db_discord;
@@ -7,6 +19,7 @@ $r = print_r($_REQUEST, true);
 $challenge = $_REQUEST['hub_challenge'];
 header("HTTP/1.1 200");
 echo $challenge;
+
 $app_access_token = $_REQUEST['app_access_token'];
 $access_token = $_REQUEST['access_token'];
 $client_id = $_REQUEST['client_id'];
@@ -41,18 +54,31 @@ $output = curl_exec($curl);
 $output_j = json_decode($output,true);
 
 $output_d = print_r($output_j,true);
+/*
+if (!empty($output_d['data'])){
+    $d = $output_d['data'][0];
+    $url = "http://localhost:13337";
+    $curl 
+    curl http://localhost:1337/ $postdata=$d
+}
+*/
+//Log and save data
+//file_put_contents("./test.txt", date('Y-m-d H:i:s') . ":\n-------------------------------------\n" . "$r\nChallenge is: $challenge\nSubs: $output_s\nStream data: $output_d\n" . "-------------------------------------\n", FILE_APPEND);
 
-if (!empty($output_j['data'])){
-    $type = $output_j['data']['type'];
+if (!empty($j['data'])){
+    $url = "http://localhost:13337";
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $data = $j['data'][0];
+    $data_string = json_encode($data);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
 
-    if($type="live"){
-        echo "Live";
-    }else{
-        echo "Not Live";
-    }
+    curl_exec($curl);
+    curl_close($curl);
 }
 
-//Log and save data
-file_put_contents("./test.txt", date('Y-m-d H:i:s') . ":\n-------------------------------------\n" . "$r\nChallenge is: $challenge\nSubs: $output_s\nStream data: $output_d\n" . "-------------------------------------\n", FILE_APPEND);
-
+file_put_contents("./test.txt", date('Y-m-d H:i:s') . ":\n-------------------------------------\n" . "$r\n". $challenge_is . "-------------------------------------\n", FILE_APPEND);
 ?>
--- announcements/store.php
+++ announcements/store.php
@@ -68,35 +68,42 @@ if($code){
             curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', "Authorization: Bearer $access_token"));
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-            $data = array("hub.callback" => "https://chukwumaokere.com/twitch/announcements/enroll.php?client_id=$client_id&app_access_token=$app_access_token&access_token=$access_token&user_id=$user_id", "hub.mode" => "subscribe", "hub.topic" => "https://api.twitch.tv/helix/streams?user_id=$user_id", "hub.lease_seconds" => 864000);
-
+            $data = array("hub.callback" => "https://chukwumaokere.com/twitch/announcements/enroll.php", "hub.mode" => "subscribe", "hub.topic" => "https://api.twitch.tv/helix/streams?user_id=$user_id", "hub.lease_seconds" => 864000);
             $data_string = json_encode($data);
             curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
 
             $output = curl_exec($curl);
 
             $output_j = json_decode($output,true);
-    
+
             /*Troubleshooting 
             var_dump($user_id);
             var_dump($preferred_name);
             var_dump($access_token);
             var_dump($app_access_token);
             */
+
+            $cb_debug="https://chukwumaokere.com/twitch/announcements/enroll.php?client_id=$client_id&app_access_token=$app_access_token&access_token=$access_token&user_id=$user_id";
+            file_put_contents("./test.txt", date('Y-m-d H:i:s') . ":\n-------------------------------------\n" . "cb_debug: $cb_debug" . "\n" . "-------------------------------------\n", FILE_APPEND);
+
             curl_close($curl);
             /*
             $success = file_get_contents("./success.php");
             echo $success;
             */
-            
             $auth_code = generateRandomString(12, $preferred_name);
-            if($output_j){
+            if(empty($output_j)){
                 $store = saveToDatabase($preferred_name, $access_token, $app_access_token, $client_id, $user_id, $auth_code);
                 if($store){
                     header("Location: ./success.php?code=$auth_code");
                 }else{
-                    echo "Something failed... please try again";
+                    echo "Query entry failed... please try again. <br> Please @Pinót in discord with this message";
                 }
+            }else{
+                $err_code = $output_j['status'];
+                $errr = $output_j['error'];
+                $mess = $output_j['message'];
+                echo "Error $err_code: $errr <br> Message: $mess <br> Please @Pinót in discord with this message";
             }
         }
     }
@@ -104,10 +111,14 @@ if($code){
 
 function saveToDatabase($preferred_name, $access_token, $app_access_token, $client_id, $user_id, $auth_code){
     global $db, $db_discord;
+    $date_entered = date('Y-m-d H:i:s');
 
-    $q1 = "INSERT INTO announcement_store (user_id, preferred_name, access_token, app_access_token, client_id) VALUES ('$user_id', '$preferred_name', '$access_token', '$app_access_token', '$client_id')";
+    $q1 = "INSERT INTO announcement_store (user_id, preferred_name, access_token, app_access_token, client_id, auth_code, date_entered) VALUES ('$user_id', '$preferred_name', '$access_token', '$app_access_token', '$client_id', '$auth_code', '$date_entered')";
     $add1 = $db->query($q1);
-    if ($add1){
+    
+    $q2 = "INSERT INTO twitch_announcements (t_user_id, t_user_name, auth_code) VALUES ('$user_id', '$preferred_name', '$auth_code')";
+    $add2 = $db_discord->query($q2);
+    if ($add1 && $add2){
         return true;    
     }else{
         return false;
