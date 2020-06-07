a58df4b9773ea803d882df58b544f39059741ca5
chukwumaokere <chukwumaokere@yahoo.com>
2020-06-07 02:31:53 +0000

    fixing bug

--- announcements/readd_all.php
+++ announcements/readd_all.php
@@ -16,7 +16,7 @@ foreach ($subs as $sub){
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_POST, 1);
     curl_setopt($curl, CURLOPT_HEADER, FALSE);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Bearer $at", "Client-ID: cr8inwbvopqkqkt9mcb31y3yqtkspp"));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', "Authorization: Bearer $at", "Client-ID: cr8inwbvopqkqkt9mcb31y3yqtkspp"));
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
     $data = array("hub.mode" => "subscribe", "hub.callback" => "https://chukwumaokere.com/twitch/announcements/enroll.php", "hub.lease_seconds" => 864000, "hub.topic" => "https://api.twitch.tv/helix/streams?user_id=$sub");
@@ -27,7 +27,7 @@ foreach ($subs as $sub){
     $output = curl_exec($curl);
     $output_p = json_decode($output);
 
-    if($output_p->status != 400){
+    if(@$output_p->status != 400){
         $cb_debug="Created subscription: " . $sub . " expires in 10 days (using: readd_all)";
         echo $cb_debug . "\n\n";
         file_put_contents("./test.txt", date('Y-m-d H:i:s') . ":\n-------------------------------------\n" . $cb_debug . "\n" . "-------------------------------------\n", FILE_APPEND);
